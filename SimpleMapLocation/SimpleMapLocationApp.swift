c50db7f08b4ca6e950aabfddf6ed6dd888875293
kenmaro3 <miha.ken.19@gmail.com>
2023-11-01 09:34:31 +0900

    Initial Commit

---
+++ SimpleMapLocation/SimpleMapLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMapLocationApp.swift
+//  SimpleMapLocation
+//
+//  Created by Kentaro Mihara on 2023/11/01.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMapLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
